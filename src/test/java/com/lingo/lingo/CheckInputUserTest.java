610fa8eb2b05026cfb6fe812b8f590fd2daffc8c
KevinKentieHBO <31728633+KevinKentieHBO@users.noreply.github.com>
2020-06-11 14:51:31 +0200

    Code Coverage with Codecov

--- src/test/java/com/lingo/lingo/CheckInputUserTest.java
+++ src/test/java/com/lingo/lingo/CheckInputUserTest.java
@@ -5,12 +5,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
+@SpringBootTest
 public class CheckInputUserTest {
 
     @ParameterizedTest
