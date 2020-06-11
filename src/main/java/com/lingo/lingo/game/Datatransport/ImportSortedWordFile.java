683e61447ad0cbf6212bccd087ded13c4d45e85c
KevinKentieHBO <31728633+KevinKentieHBO@users.noreply.github.com>
2020-06-11 17:02:32 +0200

    Resource file added to lingo game

--- src/main/java/com/lingo/lingo/game/Datatransport/ImportSortedWordFile.java
+++ src/main/java/com/lingo/lingo/game/Datatransport/ImportSortedWordFile.java
@@ -8,7 +8,7 @@ public class ImportSortedWordFile implements ImportSortedWordFileInterface {
 
     public List ReadFile(int wordLength) {
         try {
-            File file = new File("src/main/java/com/lingo/lingo/words/Recources/filteredFiles/LingoList_"+wordLength);
+            File file = new File("src/main/java/com/lingo/lingo/game/Resources/filteredFiles/LingoList_"+wordLength);
             BufferedReader br = new BufferedReader(new FileReader(file));
             List<String> Wordlist = new ArrayList<String>();
             String st;
