e425a429161a58bd9069e69a36e3be5b43f8335f
KevinKentieHBO <31728633+KevinKentieHBO@users.noreply.github.com>
2020-06-11 17:31:29 +0200

    added comment

--- src/main/java/com/lingo/lingo/game/Datatransport/ImportSortedWordFile.java
+++ src/main/java/com/lingo/lingo/game/Datatransport/ImportSortedWordFile.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class ImportSortedWordFile implements ImportSortedWordFileInterface {
 
+    //Import of the base word file depending on the amount of letters in the word.
     public List ReadFile(int wordLength) {
         try {
             File file = new File("src/main/java/com/lingo/lingo/game/Resources/filteredFiles/LingoList_"+wordLength);
